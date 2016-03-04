e5060b62745579b2d7d5eca8167f0bf3c1c20f8c
K-S-A <atillas@ukr.net>
2016-03-04 10:51:36 +0200

    Require FactoryGirl in seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'factory_girl_rails'
+
 FactoryGirl.create_list(:user, 20)     # Create list of faked users
 FactoryGirl.create_list(:post, 100)    # Seeding posts
 FactoryGirl.create_list(:comment, 400) # Randomly add comments
