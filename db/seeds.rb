2d10944274e3158d70297022a721257e45abb6f6
K-S-A <atillas@ukr.net>
2016-03-04 12:20:32 +0200

    Enable seeds in production env.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'factory_girl'
+require 'faker'
+
 FactoryGirl.create_list(:user, 20)     # Create list of faked users
 FactoryGirl.create_list(:post, 100)    # Seeding posts
 FactoryGirl.create_list(:comment, 400) # Randomly add comments
